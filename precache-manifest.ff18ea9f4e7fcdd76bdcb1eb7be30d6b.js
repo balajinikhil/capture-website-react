6ee06c4fe7758c41be08f938881748f8fe60441c
balajinikhil <balajiedward007@gmail.com>
2020-07-21 21:15:33 +0530

    Updates

---
+++ precache-manifest.ff18ea9f4e7fcdd76bdcb1eb7be30d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d47f58c1be909e29748866ff00308ec",
+    "url": "/capture-website-react/index.html"
+  },
+  {
+    "revision": "d89c6b61546677483d44",
+    "url": "/capture-website-react/static/js/2.9d6df7d8.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/capture-website-react/static/js/2.9d6df7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b733149d41b7badf33dd",
+    "url": "/capture-website-react/static/js/main.500ae46d.chunk.js"
+  },
+  {
+    "revision": "7b7fba1267085a69dac1",
+    "url": "/capture-website-react/static/js/runtime-main.7fc42f48.js"
+  }
+]);
